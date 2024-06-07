e9301db09c7569b1649bdfe38c1e9995ea372d2f
tanyavash <tanyavashistha11@gmail.com>
2024-06-07 10:13:15 +0530

    initial

---
+++ src/components/components/Hero.jsx
@@ -0,0 +1,19 @@
+import { motion } from "framer-motion";
+
+import { styles } from "../../style";
+import { ComputersCanvas } from "./canvas";
+
+
+const Hero = () => {
+  return (
+    <section className="relative w-full h-screen mx-auto">
+      
+      <div className={`${styles.paddingX}`}>
+
+      </div>
+
+    </section>
+  )
+}
+
+export default Hero
